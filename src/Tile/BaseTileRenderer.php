57f5e678c08202bfaaefa1ea01927b5978dbda90
Vincent Quatrevieux <vincent.soyuz@hotmail.fr>
2025-09-28 16:43:17 +0200

    chore: add lgpl license

--- src/Tile/BaseTileRenderer.php
+++ src/Tile/BaseTileRenderer.php
@@ -1,5 +1,21 @@
 <?php
 
+/*
+ * This file is part of PHP Map parser.
+ *
+ * PHP Map parser is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * PHP Map parser is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with PHP Map parser.
+ * If not, see <https://www.gnu.org/licenses/>.
+ *
+ * Copyright (C) 2019-2025 Vincent Quatrevieux (quatrevieux.vincent@gmail.com)
+ */
+
 namespace Arakne\MapParser\Tile;
 
 use Arakne\MapParser\Renderer\MapRenderer;
